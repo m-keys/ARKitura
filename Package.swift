09b53e2f39bd512c8bc2f768de2cc7b7af604ea7
m-keys <33558332+m-keys@users.noreply.github.com>
2019-11-07 22:36:55 +0300

    Implemented file download

--- Package.swift
+++ Package.swift
@@ -6,16 +6,17 @@ let package = Package(
     dependencies: [
       .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.8.0")),
       .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.7.1"),
+      .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", from: "1.8.3"),
       .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", from: "9.0.0"),
       .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
       .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.0"),
     ],
     targets: [
       .target(name: "ARKitura", dependencies: [ .target(name: "Application") ]),
-      .target(name: "Application", dependencies: [ "Kitura", "HeliumLogger", "CloudEnvironment","SwiftMetrics", "Health", 
+      .target(name: "Application", dependencies: [ "Kitura", "HeliumLogger", "KituraStencil", "CloudEnvironment","SwiftMetrics", "Health",
 
       ]),
 
-      .testTarget(name: "ApplicationTests" , dependencies: [.target(name: "Application"), "Kitura", "HeliumLogger" ])
+      .testTarget(name: "ApplicationTests" , dependencies: [.target(name: "Application"), "Kitura", "HeliumLogger", "KituraStencil" ])
     ]
 )
--- Sources/Application/Application.swift
+++ Sources/Application/Application.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Kitura
+import KituraStencil
 import LoggerAPI
 import Configuration
 import CloudEnvironment
@@ -12,6 +13,32 @@ public let health = Health()
 public class App {
     let router = Router()
     let cloudEnv = CloudEnv()
+    
+    var rootDirectoryPath: String {
+        let fileManager = FileManager()
+        let currentPath = fileManager.currentDirectoryPath
+        return currentPath
+    }
+    
+    var rootDirectory: URL {
+        return URL(fileURLWithPath: rootDirectoryPath)
+    }
+    
+    var publicDirectory: URL {
+        return rootDirectory.appendingPathComponent("public")
+    }
+    
+    var uploadsDirectory: URL {
+        return publicDirectory.appendingPathComponent("uploads")
+    }
+    
+    var originalsDirectory: URL {
+        return uploadsDirectory.appendingPathComponent("originals")
+    }
+    
+    var thumbsDirectory: URL {
+        return uploadsDirectory.appendingPathComponent("thumbs")
+    }
 
     public init() throws {
         // Configure logging
@@ -21,9 +48,47 @@ public class App {
     }
 
     func postInit() throws {
+        // Set Stencil as template engine
+        router.setDefault(templateEngine: StencilTemplateEngine())
+        
         // Endpoints
         initializeHealthRoutes(app: self)
-	router.all("/public", middleware: StaticFileServer())
+        
+        
+        /*for directory in [rootDirectory, publicDirectory, uploadsDirectory, originalsDirectory, thumbsDirectory] {
+            //let path = directory.path
+            let fileManager = FileManager()
+            if let allFiles = try? fileManager.contentsOfDirectory(
+                at: directory,
+                includingPropertiesForKeys: nil,
+                options: .skipsHiddenFiles) {
+                let files = allFiles.filter { !$0.path.hasSuffix("/") }
+                //allFiles.forEach { print($0.path.last ?? "nil") }
+                print(#line, #function, "Contents of directory \(directory.path):")
+                //files.forEach { print($0.lastPathComponent) }
+                files.forEach { print($0) }
+                print()
+            }
+        }*/
+        
+        //router.all("/public", middleware: StaticFileServer())
+        router.all("/pikachu", middleware: StaticFileServer(path: "\(rootDirectoryPath)/public/Pikachu"))
+        
+        router.all("/originals", middleware: StaticFileServer(path: originalsDirectory.path))
+        
+        router.get("/public*") { request, response, next in
+            defer { next() }
+            
+            let fileManager = FileManager()
+            
+            guard let files = try? fileManager.contentsOfDirectory(
+                at: self.originalsDirectory,
+                includingPropertiesForKeys: nil,
+                options: .skipsHiddenFiles
+            ) else { return }
+            
+            try response.render("list", context: ["files": files.map { $0.lastPathComponent }])
+        }
     }
 
     public func run() throws {
